1d4e5e9d0bc20e5c95826cf8792145aee3998ac0
MyHR, Andy <andy.myhr@metlife.com>
2019-06-27 16:59:35 -0400

    QaDS for running all tests, call from root of project

---
+++ bin/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Author:     MyHR, Andy <andy.myhr@metlife.com>
+# Contact:    awmyhr <awmyhr@gmail.com>
+# Project:    Shell Functions
+# Proj Home:  https://github.com/awmyhr/shell-functions
+# Copyright:  2019 awmyhr
+# License:    Apache-2.0
+# Revised:    2019-06-27
+# Created:    2019-06-27
+#==============================================================================
+
+./test/libs/bats/bin/bats test/*.bats

5adefa79445344aa98f00b55d0a64794a3b442b9
MyHR, Andy <andy.myhr@metlife.com>
2019-07-10 15:24:21 -0400

    initial version of script to run bats tests and constructor

--- bin/mkss.sh
+++ bin/mkss.sh
@@ -2,25 +2,29 @@
 #-- NOTE: Default to POSIX shell/mode. If more is needed check shell-script...
 #==============================================================================
 #:"""
-#: .. program:: mkss.sh
-#:    :synopsis: TODO: CHANGEME
+#: .. program:: make shell script (mkss.sh)
+#:    :synopsis: Run bats tests then construct script.
 #:
 #:    :copyright: 2019 awmyhr
 #:    :license: Apache-2.0
 #:
 #: .. codeauthor:: awmyhr <awmyhr@gmail.com>
 #:
-#: TODO: CHANGEME - description for documentation
+#: Given a project name, will look for a .parts file in src/. If found, will
+#: then attempt to run bats tests in test/<projectname>. If successful, will
+#: then use constructor to put the parts together into a single script.
 #:"""
 #==============================================================================
 #-- Variables which are meta for the script should be dunders (__varname__)
 #-- TODO: UPDATE meta vars
-__version__='0.1.0-alpha' #: current version
-__revised__='20190710-145755' #: date of most recent revision
+__version__='1.0.0' #: current version
+__revised__='20190710-152421' #: date of most recent revision
 __contact__='awmyhr <awmyhr@gmail.com>' #: primary contact for support/?'s
-__synopsis__='TODO: CHANGEME'
+__synopsis__='Run bats tests then construct script.'
 __description__="
-TODO: CHANGEME - description for --help
+Given a project name, will look for a .parts file in src/. If found, will
+then attempt to run bats tests in test/<projectname>. If successful, will
+then use constructor to put the parts together into a single script.
 "
 #------------------------------------------------------------------------------
 #-- Flags
@@ -32,7 +36,7 @@ __author__='awmyhr <awmyhr@gmail.com>' #: coder(s) of script
 __created__='2019-07-10'               #: date script originlly created
 __copyright__='2019 awmyhr' #: Copyright short name
 __license__='Apache-2.0'
-__cononical_name__='mkss.sh' #: static name, *NOT* os.path.basename(sys.argv[0])
+__cononical_name__='mkss' #: static name, *NOT* os.path.basename(sys.argv[0])
 __project_name__='Shell Functions'  #: name of overall project, if needed
 __project_home__='https://github.com/awmyhr/shell-functions'  #: where to find source/documentation
 __template_version__='1.2.0'             #: version of template file used
@@ -42,7 +46,7 @@ __docformat__='reStructuredText en'      #: attempted style for documentation
 __basename__="${0}" #: name script run as
 #==============================================================================
 #-- Print help or version if requested.
-if [ "${1}" = "-h" ] || [ "${1}" = "--help" ]; then
+if [ "${#}" -ne 1 ] || [ "${1}" = "-h" ] || [ "${1}" = "--help" ]; then
     printf '(%s)\n%s\n' "${__basename__}" "${__synopsis__}"
     #-- If you have options, put them here, though you may want to use shell-script instead
     printf '%s\n\n' "${__description__}"
@@ -64,6 +68,32 @@ elif [ "${__container_safe__}" = "false" ] && grep -q '/docker' /proc/self/cgrou
   exit 78
 fi
 #==============================================================================
-#-- TODO: Do something more interesting here...
-#-- Remember, this is for very simple scripts. Anything more complicated then
-#   a dozen or so lines should probably use the shell-script template...
+SCRIPT_NAME="${1}"
+SCRIPT_DIR="$(dirname "${0}")"
+if ! [ -f "src/${SCRIPT_NAME}.parts" ] ; then
+    printf 'Critical: parts file not found. Quiting.\n' 1>&2
+    exit 69
+fi
+if test -n "$(find test/"${SCRIPT_NAME}"/ -maxdepth 1 -name '*.bats' -print -quit)" ; then
+    bats test/"${SCRIPT_NAME}"/*.bats
+    EXIT_CODE="${?}"
+    if [ "${EXIT_CODE}" -ne 0 ] ; then
+        printf 'Critical: tests failed. Quiting...\n' 1>&2
+        exit "${EXIT_CODE}"
+    fi
+    printf 'Tests passed. Continuing...\n'
+else
+    printf 'Warning: no test files found. Continuing...\n' 1>&2
+fi
+
+if command -v constructor 1>/dev/null 2>&1 ; then
+    CONSTRUCTOR=constructor
+elif [ -x "${SCRIPT_DIR}/constructor.py" ] ; then
+    CONSTRUCTOR="${SCRIPT_DIR}/constructor.py"
+else
+    printf 'Critical: constructor not found. Quiting...\n' 1>&2
+    exit 69
+fi
+
+"${CONSTRUCTOR}" "src/${SCRIPT_NAME}.parts" >"${SCRIPT_NAME}.sh"
+chmod +x "${SCRIPT_NAME}.sh"
